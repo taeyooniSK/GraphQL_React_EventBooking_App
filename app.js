dd06bba1415cb8d6b0118aef112294189ad89b98
Tae-yoon <yeotaeyoon@gmail.com>
2019-06-01 23:35:47 +0900

    createEvent with dummy data:I can see how flexible graphql can be

--- app.js
+++ app.js
@@ -7,15 +7,36 @@ const { buildSchema } = require("graphql"); // buildSchema: a function that can 
 
 app.use(bodyParser.json()); 
 
+
+
+// Dummy data
+const events = [];
+
 // Set graphql config options : 
 app.use("/graphql", graphqlHttp({
     schema: buildSchema(`
+
+        type Event {
+            _id : ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String! 
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
         type RootQuery {
-            events: [String!]! 
+            events: [Event!]! 
         }
 
         type RootMutation {
-            createEvent(name: String): String,
+            createEvent(eventInput: EventInput): Event
 
         }
 
@@ -26,12 +47,20 @@ app.use("/graphql", graphqlHttp({
     `),    
     rootValue: { // javascript object where all the resolver functions are in 
         events: () => {
-            return ["Cooking", "Sailing", "Coidng all day long"];
+            return events;
         },
-        createEvent : (args) => {
-            const eventName = args.name;
-
-            return eventName;
+        createEvent : (args) => { // createEvent(resolver)'s args contains all arguments put into createEvent's arguments in mutation type
+            const event = {
+               _id: Math.random().toString(),
+               title: args.eventInput.title,    //createEvent의 argument인 eventInput은 EventInput이라는 input type형태의 테이터를 가지고 있는 객체이므로 args안에 nested되어있음
+               description: args.eventInput.description,
+               price: args.eventInput.price,
+               date: args.eventInput.date
+           };
+           
+           console.log(args);
+           events.push(event);
+           return event;
         }
     },  
     graphiql: true
