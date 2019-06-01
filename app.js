b673dede6ae20950a68db07abe11e66a35e88b75
Tae-yoon <yeotaeyoon@gmail.com>
2019-06-02 01:05:06 +0900

    Connect MongoDB with mongoose: Retrieve and Create data through graphQL

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ const bodyParser = require("body-parser");
 const app = express();
 const graphqlHttp = require("express-graphql"); // this enables me to use graphql like a middleware  in express
 const { buildSchema } = require("graphql"); // buildSchema: a function that can make me build schema literally
+const mongoose = require("mongoose");
+
+// DB model
+
+const Event = require("./models/event");
 
 
 app.use(bodyParser.json()); 
 
-
-
-// Dummy data
-const events = [];
 
 // Set graphql config options : 
 app.use("/graphql", graphqlHttp({
@@ -47,24 +48,46 @@ app.use("/graphql", graphqlHttp({
     `),    
     rootValue: { // javascript object where all the resolver functions are in 
         events: () => {
-            return events;
+            return Event.find({})
+            .then(events => {
+               console.log(events);
+               return events;
+            })
+            .catch(err => {
+                console.log(err);
+                throw err;
+            });
         },
-        createEvent : (args) => { // createEvent(resolver)'s args contains all arguments put into createEvent's arguments in mutation type
-            const event = {
-               _id: Math.random().toString(),
-               title: args.eventInput.title,    //createEvent의 argument인 eventInput은 EventInput이라는 input type형태의 테이터를 가지고 있는 객체이므로 args안에 nested되어있음
-               description: args.eventInput.description,
-               price: args.eventInput.price,
-               date: args.eventInput.date
-           };
-           
-           console.log(args);
-           events.push(event);
-           return event;
+        createEvent : args => { // createEvent(resolver)'s args contains all arguments put into createEvent's arguments in mutation type
+          const event = new Event({
+            title: args.eventInput.title,  
+            description: args.eventInput.description,
+            price: +args.eventInput.price,
+            date: new Date(args.eventInput.date)
+          });
+          // save event in database
+          return event.save()
+          .then(result => {
+              console.log(result);
+              return result;
+          })
+          .catch(err => {
+              console.log(err);
+              throw err;
+          })
         }
     },  
     graphiql: true
 }));
+
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-tdceq.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`, { useNewUrlParser: true })
+.then(() => {
+    console.log("MongoDB connected..");
+})
+.catch(err => {
+    console.log(err);
+})
 
 app.listen(3000, ()=>{
     console.log("Server is running on 3000");
