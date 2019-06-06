5171304341cb9aa91b47f86ae2f7bad7c5d2f7a3
Tae-yoon <yeotaeyoon@gmail.com>
2019-06-06 16:24:47 +0900

    Add Navbar with some styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,23 @@ import React from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import AuthPage from "./pages/Auth";    
 import BookingsPage from "./pages/Bookings";    
-import EventsPage from "./pages/Events";    
+import EventsPage from "./pages/Events";
+import MainNavigation from "./components/Navigation/MainNavigation";    
 
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Redirect from="/" to="/auth/" exact />
-        <Route path="/auth" component={AuthPage} />
-        <Route path="/events" component={EventsPage} />
-        <Route path="/bookings" component={BookingsPage} />
-      </Switch>
+      <MainNavigation />
+      <main className="main-content">
+        <Switch>
+          <Redirect from="/" to="/auth/" exact />
+          <Route path="/auth" component={AuthPage} />
+          <Route path="/events" component={EventsPage} />
+          <Route path="/bookings" component={BookingsPage} />
+        </Switch>
+      </main>
     </BrowserRouter>
   );
 }
